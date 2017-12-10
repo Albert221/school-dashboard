0428e8c070d51b8bb8af0afb6eb4f4750b62b9ac
Jakub Bizewski <jakubbizewski@gmail.com>
2017-12-10 11:35:59 +0100

    Add updater and change default background.

---
+++ bin/updater/update.js
@@ -0,0 +1,57 @@
+var fs = require('fs')
+var fetch = require('node-fetch')
+var child_process = require('child_process')
+
+var url = 'https://api.github.com/repos/PozytywneInicjatywy/dashboard-front/commits'
+
+var params = [
+    '../../index.html',
+    '--kiosk',
+    '--incognito',
+    '--disable-infobars'
+]
+
+function runCommand(command, cwd) {
+    try {
+        child_process.execSync(command)
+    }
+    catch (exception) {
+
+    }
+}
+
+fetch(url).then(function(response) {
+    response.json().then(function(data) {
+        var sha = data[0]['sha']
+
+        fs.readFile('./lastver.txt', 'utf8', function (error, data) {
+            if (error || data !== sha) {
+                console.log('error or outdated')
+
+                runCommand('pkill chrome')
+
+                runCommand('cp ../../src/constants.js /tmp/dashboard-backup.js')
+
+                runCommand('git fetch --all')
+                runCommand('git reset --hard origin/master')
+
+                runCommand('mv /tmp/dashboard-backup.js ../../src/constants.js')
+
+                runCommand('npm install')
+                runCommand('npm run build')
+
+                child_process.spawn('google-chrome', params, { detached: true, stdio: 'ignore', env: { DISPLAY: ':0' } })
+                    .unref()
+
+                fs.writeFile('./lastver.txt', sha, 'utf8', function (error) {
+                    if (error) {
+                        console.log(error)
+                    }
+                })
+            }
+            else {
+                console.log('all is good')
+            }
+        })
+    })
+})
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const OWM_APPID = process.env.OWM_APPID
 export const API_URL = 'http://localhost:8081/api'
 
-export const BACKGROUND_TYPE = 'twitch'
-export const BACKGROUND_SRC = 'esl_csgo'
+export const BACKGROUND_TYPE = 'image'
+export const BACKGROUND_SRC = 'https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?dl&fit=crop&w=1920&h=1280'
