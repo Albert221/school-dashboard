6a0ef6e494ae11ada0783c6f5436a3455f48d375
Albert221 <w.albert221@gmail.com>
2017-12-13 21:23:56 +0100

    Change API URL to prod one

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const OWM_APPID = process.env.OWM_APPID
-export const API_URL = 'http://localhost:8081/api'
+export const API_URL = 'https://pozytywne-dashboard.wolszon.me/api'
 
 export const BACKGROUND_TYPE = 'image'
 export const BACKGROUND_SRC = 'https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?dl&fit=crop&w=1920&h=1280'
