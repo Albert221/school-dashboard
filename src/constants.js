84decdf41fc3d27729b3f53cae37ae35a2bc39ca
Albert221 <w.albert221@gmail.com>
2017-12-15 20:32:37 +0100

    Set background image to Christmas one

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const OWM_APPID = process.env.OWM_APPID
 export const API_URL = 'https://pozytywne-dashboard.wolszon.me/api'
 
 export const BACKGROUND_TYPE = 'image'
-export const BACKGROUND_SRC = 'https://images.pexels.com/photos/39811/pexels-photo-39811.jpeg?dl&fit=crop&w=1920&h=1280'
+export const BACKGROUND_SRC = 'https://images.pexels.com/photos/257909/pexels-photo-257909.jpeg?dl&fit=crop&w=1920&h=1280'
